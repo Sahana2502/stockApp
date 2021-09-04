5754491bf5e355ab2fde6f4b1949989d7cecf04c
SahanaVenkatesh <sahana.venkatesh05@gmail.com>
2021-09-03 20:49:17 -0400

    1.Implemented the StockList activity to display available stocks 2.The activity updates the value with real time data for every 10 seconds.

--- app/src/main/java/com/pega/stockapp/activities/StockListActivity.java
+++ app/src/main/java/com/pega/stockapp/activities/StockListActivity.java
@@ -1,16 +1,152 @@
 package com.pega.stockapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.pega.stockapp.R;
+import com.pega.stockapp.adapter.StockAdapter;
+import com.pega.stockapp.model.Stock;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class StockListActivity extends AppCompatActivity {
+    private OkHttpClient client;
+    private ArrayList<Stock> stocks;
+    private StockAdapter stockAdapter;
+    private RecyclerView recyclerView;
+    private static final String STOCK_API = "https://71iztxw7wh.execute-api.us-east-1.amazonaws.com/interview/favorite-stocks";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_list);
+        setUpRecyclerView();
+
     }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        scheduleTimerTask();
+    }
+
+    private void scheduleTimerTask() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                getStocks();
+            }
+        }, 0, 10000);
+    }
+
+    private void getStocks() {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(STOCK_API)
+                .build();
+        try {
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(!isFinishing() && hasWindowFocus())
+                                displayAlert();
+                        }
+                    });
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    displayStocks(response);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayStocks(Response response) {
+        String companySymbol, companyName = null;
+        double currentPrice = 0, lowPrice = 0, highPrice = 0;
+        Stock stock;
+        stocks = new ArrayList<>();
+        JSONObject responseObject = null;
+        try {
+            responseObject = new JSONObject(response.body().string());
+        } catch (JSONException | IOException jsonException) {
+            jsonException.printStackTrace();
+        }
+        Iterator keys = responseObject.keys();
+        while (keys.hasNext()) {
+            companySymbol = (String) keys.next();
+            try {
+                companyName = (String) responseObject.getJSONObject(companySymbol).get("name");
+                currentPrice = (double) responseObject.getJSONObject(companySymbol).getDouble("price");
+                lowPrice = (double) responseObject.getJSONObject(companySymbol).getDouble("low");
+                highPrice = (double) responseObject.getJSONObject(companySymbol).getDouble("high");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            stock = new Stock(companySymbol, companyName, currentPrice, lowPrice, highPrice);
+            stocks.add(stock);
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                updateRecords();
+            }
+        });
+    }
+
+    public void displayAlert() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(StockListActivity.this);
+        alertBuilder.setMessage(getResources().getString(R.string.error));
+        alertBuilder.setCancelable(true);
+
+        alertBuilder.setPositiveButton(
+                "GOT IT",
+                (dialog, id) -> dialog.cancel());
+
+        AlertDialog alertDialog = alertBuilder.create();
+        alertDialog.show();
+    }
+
+    private void setUpRecyclerView() {
+        recyclerView = findViewById(R.id.rv_stocks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(stockAdapter);
+    }
+
+    private void updateRecords() {
+        stockAdapter = new StockAdapter(stocks);
+        recyclerView.setAdapter(stockAdapter);
+    }
+
+
 }
