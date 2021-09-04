86fdd34acbdb1aca7ab7d96811c05cf84a5a2c94
SahanaVenkatesh <sahana.venkatesh05@gmail.com>
2021-09-03 20:24:47 -0400

    Added model and adapter class files

--- app/src/main/java/com/pega/stockapp/adapter/StockAdapter.java
+++ app/src/main/java/com/pega/stockapp/adapter/StockAdapter.java
@@ -1,4 +1,69 @@
 package com.pega.stockapp.adapter;
 
-public class StockAdapter {
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.pega.stockapp.R;
+import com.pega.stockapp.activities.StockDetailActivity;
+import com.pega.stockapp.model.Stock;
+
+import java.util.ArrayList;
+
+public class StockAdapter extends RecyclerView.Adapter<StockAdapter.StockViewHolder> {
+
+    private final ArrayList<Stock> stocks;
+
+    public StockAdapter(ArrayList<Stock> stocks) {
+        this.stocks = stocks;
+    }
+
+    @NonNull
+    @Override
+    public StockViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stock_item, parent, false);
+        return new StockViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StockViewHolder holder, int position) {
+        Stock stock = stocks.get(position);
+        holder.companySymbol.setText(stock.getCompanySymbol());
+        holder.companyName.setText(stock.getCompanyName());
+        holder.currentStockPrice.setText(String.format("%.2f", stock.getCurrentStockPrice()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return stocks.size();
+    }
+
+    public class StockViewHolder extends RecyclerView.ViewHolder {
+        private final TextView companySymbol;
+        private final TextView companyName;
+        private final TextView currentStockPrice;
+
+        public StockViewHolder(@NonNull View itemView) {
+            super(itemView);
+            companySymbol = itemView.findViewById(R.id.text_company_symbol);
+            companyName = itemView.findViewById(R.id.text_company_name);
+            currentStockPrice = itemView.findViewById(R.id.text_stock_price);
+
+            itemView.setOnClickListener(view -> {
+                Intent intent = new Intent(itemView.getContext(), StockDetailActivity.class);
+                intent.putExtra("Stock Object", stocks.get(getAdapterPosition()));
+                itemView.getContext().startActivity(intent);
+            });
+
+        }
+
+
+    }
 }
--- app/src/main/java/com/pega/stockapp/model/Stock.java
+++ app/src/main/java/com/pega/stockapp/model/Stock.java
@@ -1,4 +1,100 @@
 package com.pega.stockapp.model;
 
-public class Stock {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Stock implements Parcelable {
+    private String companySymbol, companyName;
+    private double currentStockPrice, dailyLowPrice, dailyHighPrice;
+
+    public Stock() {
+
+    }
+
+    public Stock(String companySymbol, String companyName, double currentStockPrice, double dailyLowPrice, double dailyHighPrice) {
+        this.companySymbol = companySymbol;
+        this.companyName = companyName;
+        this.currentStockPrice = currentStockPrice;
+        this.dailyLowPrice = dailyLowPrice;
+        this.dailyHighPrice = dailyHighPrice;
+    }
+
+    protected Stock(Parcel in) {
+        companySymbol = in.readString();
+        companyName = in.readString();
+        currentStockPrice = in.readDouble();
+        dailyLowPrice = in.readDouble();
+        dailyHighPrice = in.readDouble();
+    }
+
+    public static final Creator<Stock> CREATOR = new Creator<Stock>() {
+        @Override
+        public Stock createFromParcel(Parcel in) {
+            return new Stock(in);
+        }
+
+        @Override
+        public Stock[] newArray(int size) {
+            return new Stock[size];
+        }
+    };
+
+
+    public String getCompanySymbol() {
+        return companySymbol;
+    }
+
+    public void setCompanySymbol(String companySymbol) {
+        this.companySymbol = companySymbol;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public double getCurrentStockPrice() {
+        return currentStockPrice;
+    }
+
+    public void setCurrentStockPrice(double currentStockPrice) {
+        this.currentStockPrice = currentStockPrice;
+    }
+
+    public double getDailyLowPrice() {
+        return dailyLowPrice;
+    }
+
+    public void setDailyLowPrice(double dailyLowPrice) {
+        this.dailyLowPrice = dailyLowPrice;
+    }
+
+    public double getDailyHighPrice() {
+        return dailyHighPrice;
+    }
+
+    public void setDailyHighPrice(double dailyHighPrice) {
+        this.dailyHighPrice = dailyHighPrice;
+    }
+
+    public static Creator<Stock> getCREATOR() {
+        return CREATOR;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(companySymbol);
+        parcel.writeString(companyName);
+        parcel.writeDouble(currentStockPrice);
+        parcel.writeDouble(getDailyLowPrice());
+        parcel.writeDouble(getDailyHighPrice());
+    }
 }
